a9da128e3647b4790591c2b163f8333982dd0a2b
Vadim Yastrebov <5952749+CFD2@users.noreply.github.com>
2019-06-28 15:40:21 -0400

    threads only

--- mdtest_18.04.sh
+++ mdtest_18.04.sh
@@ -13,15 +13,15 @@ touch $LOGNAME
 #numFiles=(1000 10000 50000 100000 250000 500000 750000 1000000)
 numFiles=(1000 10000 50000 100000)
 #	numFiles=(1000 10000)
-for i in "${numFiles[@]}"
-do
-	echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" >> $LOGNAME
-	echo "Starting MDTest 1 thread, numFilesTotal="$i >> $LOGNAME
-	echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" >> $LOGNAME
-	mdtest -n=$i -i=10 -u --posix.odirect 2>&1 | tee -a $LOGNAME
-	echo "sleeping for 15s"
-	sleep 15s
-done 
+#for i in "${numFiles[@]}"
+#do
+#	echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" >> $LOGNAME
+#	echo "Starting MDTest 1 thread, numFilesTotal="$i >> $LOGNAME
+#	echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" >> $LOGNAME
+#	mdtest -n=$i -i=10 -u --posix.odirect 2>&1 | tee -a $LOGNAME
+#	echo "sleeping for 15s"
+#	sleep 15s
+#done 
 #numThreads=(3 4 8 16 32 64 128 256 512)
 numThreads=(3 4 8 12 16 20)
 #numThreads=(8 16 32 64 128 256 512)
